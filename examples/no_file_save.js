fc848c34a73fb270bd993439b2e1f54daa9ec247
Дмитрий Луканин <dylukanin@gmail.com>
2017-03-28 09:40:00 +0300

    added no file save raspistill example

--- examples/no_file_save.js
+++ examples/no_file_save.js
@@ -8,8 +8,6 @@ const raspistill = new Raspistill({
 raspistill.takePhoto('first')
     .then((photo) => {
         console.log('took first photo', photo);
-        fs.writeFile('message.txt', 'Hello Node.js', 'utf8', callback);
-
     })
     .catch((error) => {
         console.error('something bad happened', error);
